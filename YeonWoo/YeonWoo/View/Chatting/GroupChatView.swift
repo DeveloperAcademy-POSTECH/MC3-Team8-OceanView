ac78d6ebaa36582383ea808613ea4db7f1caf4ed
보경 <97583162+bokoo14@users.noreply.github.com>
2023-07-19 14:30:50 +0900

    [Feat] #3 - 채팅방 textfield 생성 및 ScrollView구현

--- YeonWoo/YeonWoo/View/Chatting/GroupChatView.swift
+++ YeonWoo/YeonWoo/View/Chatting/GroupChatView.swift
@@ -8,8 +8,61 @@
 import SwiftUI
 
 struct GroupChatView: View {
+    @State var message: String = ""
+    @State var missionTitle: String = "영일대에서 정장입고 수영하기"
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color("Main02").ignoresSafeArea()
+            
+            VStack (spacing: 0){
+                
+                roundedInfinityWidthBox(boxHeight: 40, boxBackgroundColor: "Point01") {
+                    RoundedRectangle(cornerRadius: 4)
+                        .inset(by: 1)
+                        .stroke(.black, lineWidth: 2)
+                        .overlay {
+                            Text(missionTitle)
+                                .foregroundColor(Color("GrayScale10"))
+                                .font(Font.custom("Pretendard-Bold", size: 20))
+                        }
+                }
+                .padding(.horizontal, 16)
+                .padding(.bottom, 28)
+                
+                ScrollView{
+                    VStack (spacing: 0){
+                        Text("미션이 시작되었습니다.")
+                            .font(.system(size: 15, weight: .bold))
+                            .foregroundColor(Color("Main01"))
+                        Text("새로운 우연과 미션을 수행해주세요")
+                            .font(Font.custom("Pretendard-Bold", size: 13))
+                            .foregroundColor(Color("Main01"))
+                    }
+                    
+                    // 상대방일때
+                    
+                }
+                
+                Spacer()
+                HStack (spacing: 0){
+                    roundedInfinityWidthBox(boxHeight: 40, boxBackgroundColor: "GrayScale10") {
+                        TextField("메세지를 입력하세요", text: $message)
+                            .padding()
+                    }
+                    .padding(.horizontal, 16)
+                    
+                    roundedFixedSizeBox(boxWidth: 40, boxHeight: 40, boxBackgroundColor: "Main01") {
+                        Button {
+                            // action
+                        } label: {
+                            Text("💌")
+                        }
+                    }
+                    .padding(.trailing, 16)
+                } // HStack
+                
+            } // VStack
+        } // ZStack
     }
 }
 
