b893613b2ff4bdd1df09b070d886900028a94cbd
보경 <97583162+bokoo14@users.noreply.github.com>
2023-07-19 12:16:41 +0900

    [Feat] #2 - 채팅방 상단 채팅방 RoundedRectangle 구현

--- YeonWoo/YeonWoo/View/Chatting/ChatListView.swift
+++ YeonWoo/YeonWoo/View/Chatting/ChatListView.swift
@@ -11,17 +11,33 @@ struct ChatListView: View {
     var body: some View {
         NavigationView {
             VStack {
-                Text("고정~")
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 40)
+                    .background(Color("Point01"))
+                    .cornerRadius(4)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 4)
+                            .inset(by: 1)
+                            .stroke(.black, lineWidth: 2)
+                            .overlay {
+                                Text("채팅방")
+                                    .foregroundColor(Color("GrayScale10"))
+                                    .font(Font.custom("Pretendard-Bold", size: 20))
+                            }
+                    )
+                    .padding(.horizontal, 16)
+                    .padding(.vertical, 10)
+                
                 ScrollView {
-                    chatList(imageTitle: "Imgprofile", missionTitle: "영일대에서 키위먹고 수영하기", lastChat: "안녕하세요. 저는 골드키위입니다 반기워요!", timeInfo: "므 8:26")
-                    chatList(imageTitle: "Imgprofile", missionTitle: "영일대에서 키위먹고 수영하기", lastChat: "안녕하세요. 저는 골드키위입니다 반기워요!", timeInfo: "므 8:26")
-                    chatList(imageTitle: "Imgprofile", missionTitle: "영일대에서 키위먹고 수영하기", lastChat: "안녕하세요. 저는 골드키위입니다 반기워요!", timeInfo: "므 8:26")
-                    chatList(imageTitle: "Imgprofile", missionTitle: "영일대에서 키위먹고 수영하기", lastChat: "안녕하세요. 저는 골드키위입니다 반기워요!", timeInfo: "므 8:26")
+                    chatList(imageTitle: "Imgprofile", missionTitle: "영일대에서 키위먹고 수영하기", lastChat: "안녕하세요. 저는 골드키위입니다 반기워요!", timeInfo: "AM 8:26")
+                    chatList(imageTitle: "Imgprofile", missionTitle: "영일대에서 키위먹고 수영하기", lastChat: "안녕하세요. 저는 골드키위입니다 반기워요!", timeInfo: "PM 8:26")
+                    chatList(imageTitle: "Imgprofile", missionTitle: "영일대에서 키위먹고 수영하기", lastChat: "안녕하세요. 저는 골드키위입니다 반기워요!", timeInfo: "AM 8:26")
+                    chatList(imageTitle: "Imgprofile", missionTitle: "영일대에서 키위먹고 수영하기", lastChat: "안녕하세요. 저는 골드키위입니다 반기워요!", timeInfo: "AM 8:26")
                 } // ScrollView
-            }
+            } // VStack
             
-            //.navigationTitle("채팅방")
-            //.navigationBarTitleDisplayMode(.inline)
         } // NavigationView
         .tint(.black)
         
